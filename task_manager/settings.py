7896f5b562aeb4bf3476c45758ba8ab9293237b8
Aleksei Semenov <alek753@yandex.ru>
2025-03-26 14:25:03 +0400

    fixing settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +34,7 @@ ALLOWED_HOSTS = [
     'webserver',
     'localhost',
     '127.0.0.1',
+    'python-project-52-oyvr.onrender.com',
 ]
 
 

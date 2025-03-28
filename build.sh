4cf624cfe8eae767625b4ca5ca64d17cb581ccf7
Aleksei Semenov <alek753@yandex.ru>
2025-03-28 14:48:42 +0400

    fixing build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
+source $HOME/.local/bin/env
+
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
 # make install && make collectstatic && make migrate

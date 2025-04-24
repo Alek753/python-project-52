b0b5896d500a5b5ee7296752358721b609f5fbeb
Aleksei Semenov <alek753@yandex.ru>
2025-04-24 16:32:35 +0400

    added labels

---
+++ task_manager/labels/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from task_manager.labels import views
+
+
+app_name = "labels"
+
+
+urlpatterns = [
+    path('', views.LabelsListView.as_view(), name='list'),
+#    path('create/', views.LabelsCreateView.as_view(), name='create'),
+#    path('<int:pk>/update/', views.LabelsUpdateView.as_view(), name='update'),
+#    path('<int:pk>/delete/', views.LabelsDeleteView.as_view(), name='delete'),
+]
---
+++ task_manager/labels/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView,DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Label
+from .forms import LabelCreateForm
+
+# Create your views here.
+class LabelsListView(ListView):
+    model = Label
+    template_name = 'labesl/labels_list.html'
+    context_object_name = 'labels'
+
+class LabelCreateView(SuccessMessageMixin, CreateView):
+    model = Label
+    form_class = LabelCreateForm
+    template_name = 'labels/label_create.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = 'Метка успешно создана!'
+
+class LabelUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = Label
+    form_class = LabelCreateForm
+    template_name = 'labels/update.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = 'Статус успешно отредактирован!'
+
+class LabelDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
+    model = Label
+    template_name = 'labels/delete.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = 'Статус успешно удален!'

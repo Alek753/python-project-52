dc823998e816ed37c057fda05acfe52bfc5fda7a
Alek753 <119520405+Alek753@users.noreply.github.com>
2025-06-03 22:35:49 +0400

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -155,6 +155,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 ROLLBAR = {
     'access_token': os.getenv("ROLLBAR_ACCESS_TOKEN"),
     'environment': 'development' if DEBUG else 'production',
